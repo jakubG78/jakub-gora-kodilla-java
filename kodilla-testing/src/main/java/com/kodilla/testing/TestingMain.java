b4513b4667068f844c80ed2b37ee3f69f740fed9
jakubG78 <jakub.gora@wp.pl>
2020-01-13 20:16:59 +0100

    Commit for m06.01

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,7 @@
+package com.kodilla.testing;
+
+public class TestingMain {
+    public static void main(String[] arg){
+        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    }
+}
