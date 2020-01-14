52042e7ceb791e642a35b9ff241ae2f0c1131947
jakubG78 <jakub.gora@wp.pl>
2020-01-14 20:35:44 +0100

    Commit for m06.02

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] arg){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("test Ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy - kalkulatora:");
+
+        Calculator calculator = new Calculator();
+
+        Integer addResult = calculator.addNumbers(5,3);
+        if(addResult == 8){
+            System.out.println("Adding 5 to 3 result correct equals: " + addResult + ".");
+        } else {
+            System.out.println("Adding procedure error.");
+        }
+
+        Integer subResult = calculator.subtractNumbers(25,3);
+        if(subResult == 22){
+            System.out.println("Subtracting 3 form 25 result correct equals: " + subResult + ".");
+        } else {
+            System.out.println("Subtracting procedure error.");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public Integer addNumbers (Integer numberA, Integer numberB) {
+        return numberA + numberB;
+    }
+    public Integer subtractNumbers(Integer numberA, Integer numberB) {
+        return numberA + numberB;
+    }
+}
