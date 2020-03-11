fcea0abc9e356cc781ed997819392e3fae43f586
jakubG78 <jakub.gora@wp.pl>
2020-03-11 22:32:51 +0100

    Commit for m13.04 v2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/OrderProcessor.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/OrderProcessor.java
@@ -3,25 +3,30 @@ package com.kodilla.good.patterns.challenges.food2door;
 public class OrderProcessor {
 
     private FoodOrderService foodOrderService;
+    private FoodOrderRequest foodOrderRequest;
     private FoodOrderRepository foodOrderRepository;
+
 
     public OrderProcessor(final FoodOrderRepository foodOrderRepository) {
         this.foodOrderRepository = foodOrderRepository;
     }
 
     public FoodOrderDto process(final FoodSupplier foodSupplier) {
-        FoodOrderRequest foodOrderRequest = new FoodOrderRequest(foodSupplier, null);
+        foodOrderRequest = new FoodOrderRequest(foodSupplier, null);
         if (foodSupplier.getName().equals("ExtraFoodShop")) {
             ExtraFoodShopOrderRequestRetriver extraFoodShopOrderRequestRetriver = new ExtraFoodShopOrderRequestRetriver();
             foodOrderRequest = extraFoodShopOrderRequestRetriver.retrive();
+            foodOrderService = new ExtraFoodShopOrderService();
         } else {
             if (foodSupplier.getName().equals("HealthyShop")) {
                 HealthyShopOrderRequestRetriver healthyShopOrderRequestRetriver = new HealthyShopOrderRequestRetriver();
                 foodOrderRequest = healthyShopOrderRequestRetriver.retrive();
+                foodOrderService = new HealthyShopOrderService();
             } else {
                 if (foodSupplier.getName().equals("GlutenFreeShop")) {
                     GlutenFreeShopOrderRequestRetriver glutenFreeShopOrderRequestRetriver = new GlutenFreeShopOrderRequestRetriver();
                     foodOrderRequest = glutenFreeShopOrderRequestRetriver.retrive();
+                    foodOrderService = new GlutenFreeShopOrderService();
                 }
             }
         }
