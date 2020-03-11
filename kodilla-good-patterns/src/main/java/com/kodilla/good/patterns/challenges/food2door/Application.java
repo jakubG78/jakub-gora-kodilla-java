f4f100c8bcc2b20b580c67fe090f16df47c3cfb4
jakubG78 <jakub.gora@wp.pl>
2020-03-11 22:18:50 +0100

    Commit for m13.04 v2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/Application.java
@@ -2,23 +2,9 @@ package com.kodilla.good.patterns.challenges.food2door;
 
 public class Application {
     public static void main(String[] args) {
-
-        ExtraFoodShopOrderRequestRetriver extraFoodShopOrderRequestRetriver = new ExtraFoodShopOrderRequestRetriver();
-        FoodOrderRequest extraFoodShopOrderRequest = extraFoodShopOrderRequestRetriver.retrive();
-
-        HealthyShopOrderRequestRetriver healthyShopOrderRequestRetriver = new HealthyShopOrderRequestRetriver();
-        FoodOrderRequest healthyShopOrderRequest = healthyShopOrderRequestRetriver.retrive();
-
-        GlutenFreeShopOrderRequestRetriver glutenFreeShopOrderRequestRetriver = new GlutenFreeShopOrderRequestRetriver();
-        FoodOrderRequest glutenFreeShopOrderRequest = glutenFreeShopOrderRequestRetriver.retrive();
-
-        OrderProcessor extraFoodShopOrderProcessor = new OrderProcessor(new ExtraFoodShopOrderService(), new WebFoodOrderRepository());
-        extraFoodShopOrderProcessor.process(extraFoodShopOrderRequest);
-
-        OrderProcessor healthyShopOrderProcessor = new OrderProcessor(new HealthyShopOrderService(), new WebFoodOrderRepository());
-        healthyShopOrderProcessor.process(healthyShopOrderRequest);
-
-        OrderProcessor glutenFreeShopOrderProcessor = new OrderProcessor(new GlutenFreeShopOrderService(), new WebFoodOrderRepository());
-        glutenFreeShopOrderProcessor.process(glutenFreeShopOrderRequest);
+        OrderProcessor orderProcessor = new OrderProcessor(new ExtraFoodShopOrderService(), new WebFoodOrderRepository());
+        orderProcessor.process(new FoodSupplier("ExtraFoodShop"));
+        orderProcessor.process(new FoodSupplier("HealthyShop"));
+        orderProcessor.process(new FoodSupplier("GlutenFreeShop"));
     }
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/OrderProcessor.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/OrderProcessor.java
@@ -10,7 +10,23 @@ public class OrderProcessor {
         this.foodOrderRepository = foodOrderRepository;
     }
 
-    public FoodOrderDto process(final FoodOrderRequest foodOrderRequest) {
+    public FoodOrderDto process(final FoodSupplier foodSupplier) {
+        FoodOrderRequest foodOrderRequest = new FoodOrderRequest(foodSupplier, null);
+        if (foodSupplier.getName().equals("ExtraFoodShop")) {
+            ExtraFoodShopOrderRequestRetriver extraFoodShopOrderRequestRetriver = new ExtraFoodShopOrderRequestRetriver();
+            foodOrderRequest = extraFoodShopOrderRequestRetriver.retrive();
+        } else {
+            if (foodSupplier.getName().equals("HealthyShop")) {
+                HealthyShopOrderRequestRetriver healthyShopOrderRequestRetriver = new HealthyShopOrderRequestRetriver();
+                foodOrderRequest = healthyShopOrderRequestRetriver.retrive();
+            } else {
+                if (foodSupplier.getName().equals("GlutenFreeShop")) {
+                    GlutenFreeShopOrderRequestRetriver glutenFreeShopOrderRequestRetriver = new GlutenFreeShopOrderRequestRetriver();
+                    foodOrderRequest = glutenFreeShopOrderRequestRetriver.retrive();
+                }
+            }
+        }
+
         boolean isOrderCompleted = foodOrderService.order(foodOrderRequest.getFoodSupplier(), foodOrderRequest.getFoodItem());
 
         if (isOrderCompleted) {
