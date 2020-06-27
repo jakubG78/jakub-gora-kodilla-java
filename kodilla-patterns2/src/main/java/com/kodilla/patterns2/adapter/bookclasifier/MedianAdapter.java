1a8b4afe4bf6bad15898bee83f4e342038082924
jakubG78 <jakub.gora@wp.pl>
2020-06-27 17:01:34 +0200

    Commit for m24.05 MedianAdapter update

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
@@ -1,6 +1,5 @@
 package com.kodilla.patterns2.adapter.bookclasifier;
 
-import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
 import com.kodilla.patterns2.adapter.bookclasifier.librarya.Classifier;
 import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
 
@@ -11,9 +10,9 @@ import java.util.Set;
 public class MedianAdapter extends MedianAdaptee implements Classifier {
 
     @Override
-    public int publicationYearMedian(Set<Book> bookSet) {
+    public int publicationYearMedian(Set<com.kodilla.patterns2.adapter.bookclasifier.librarya.Book> bookSet) {
         Map<BookSignature, com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book> books = new HashMap<>();
-        for (Book theBook : bookSet) {
+        for (com.kodilla.patterns2.adapter.bookclasifier.librarya.Book theBook : bookSet) {
             books.put(new BookSignature(theBook.getSignature()), new com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book(
                     theBook.getAuthor(),
                     theBook.getTitle(),
